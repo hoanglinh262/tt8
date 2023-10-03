a4c1c9cdcaf3a1652b305ad24deafe2f464691ba
hoanglinh262 <144077835+hoanglinh262@users.noreply.github.com>
2023-10-03 10:45:56 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+		Scanner sc = new Scanner(System.in);
+		String Name = "Le Hoang Linh";
+		 System.out.println ("hoten:"+Name);
+		 int maSV =sc.nextInt();
+		 System.out.println ("maSv:"+maSV);
+		 long cmnd = sc.nextLong();
+		 System.out.println ("cmnd"+cmnd);
+		 float dtb = sc.nextFloat();
+		 System.out.println ("dtb:"+dtb);
+		 String name = "Le Van A";
+		 System.out.println ("name:"+name);
+		 long sdt =sc.nextLong();
+		 System.out.println ("sdt:"+sdt);
+		 float hsl = sc.nextFloat();
+		 System.out.println ("hsl:"+hsl);
+		 String nameCar = "Maserati M20";
+		 System.out.println ("namecar:"+nameCar);
+		 float gia = sc.nextFloat();
+		 System.out.println ("gia:"+gia);
+		 String color = "Blue";
+		 System.out.println ( "Color:"+color);
+	}
+
+}
